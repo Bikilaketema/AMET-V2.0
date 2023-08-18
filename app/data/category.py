da23f488baa204431c75016e10f5bf4d6b07a648
bikilaketema <bikilaketema94@gmail.com>
2023-08-19 00:11:04 +0300

    Added responsivenness feature for mobile and Desktop made the website take it's data locally from python and json files Now there is no need of firebase and fakestore api. It handles everthing with in it's self.
    But still does not have a Database

---
+++ app/data/category.py
@@ -0,0 +1,14 @@
+categories_data = [
+{'name': 'Accessories', 'image': '/static/icons/accessories.png'},
+{'name': 'Beauty', 'image': '/static/icons/beauty.png'},
+{'name': 'Electronics', 'image': '/static/icons/electronics.png'},
+{'name': 'Home', 'image': '/static/icons/home.png'},
+{'name': "Men's Clothing", 'image': '/static/icons/mens_clothing.png'},
+{'name': 'Outdoors', 'image': '/static/icons/outdoors.png'},
+{'name': 'Sports', 'image': '/static/icons/sports.png'},
+{'name': 'Video Games', 'image': '/static/icons/video_games.png'},
+{'name': 'Watches', 'image': '/static/icons/watches.png'},
+{'name': 'Yoga & Fitness', 'image': '/static/icons/yoga_fitness.png'},
+]
+
+
